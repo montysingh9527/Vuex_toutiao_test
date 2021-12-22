bd4c3585a330851034a123f3f69c65b4070924c8
montysingh <montysingh9527@gmail.com>
2021-12-22 10:58:38 +0800

    Vuex-新增home/about模块

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,22 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import home from './modules/home'
+import about from './modules/about'
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  state: {
+
+  },
+  mutations: {
+
+  },
+  actions: {
+
+  },
+  modules: {
+    home,
+    about
+  }
 });
